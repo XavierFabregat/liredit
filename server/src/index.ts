e694b3c019e83f01cc278673495ea153e597d2fd
XavierFabregat <xavi.fabregat.pous@gmail.com>
2023-06-28 12:58:49 +0200

    [BE/Fix]: Add client origin t cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -39,7 +39,7 @@ const main = async () => {
 
   app.use(
     cors({
-      origin: "https://studio.apollographql.com",
+      origin: ["https://studio.apollographql.com", "http://localhost:3000"],
       credentials: true,
     })
   );
