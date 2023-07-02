f458d49f69177dfb8c94e670ad2f6c88dfc6d2f9
XavierFabregat <xavi.fabregat.pous@gmail.com>
2023-07-02 23:49:39 +0200

    Not gonna lie, this is just to run the action

--- server/src/types.ts
+++ server/src/types.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import { Redis } from "ioredis";
 
+// This is a type definition for the context object that is passed to all resolvers.
+
 export type MyContext = {
   req: Request;
   res: Response;
