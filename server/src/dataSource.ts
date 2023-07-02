97cca6251eea0a3ca70693d2d842eac0338dc235
XavierFabregat <xavi.fabregat.pous@gmail.com>
2023-07-02 23:25:40 +0200

    [BE/refactor]: Remove more console logs

--- server/src/dataSource.ts
+++ server/src/dataSource.ts
@@ -3,7 +3,8 @@ import { __prod__ } from "./constants";
 import { User, Post } from "./entities";
 import path from "path";
 
-const { DB,
+const {
+  DB,
   ELEPHANTSQL_DB,
   ELEPHANTSQL_HOST,
   ELEPHANTSQL_PASSWORD,
@@ -11,40 +12,29 @@ const { DB,
   ELEPHANTSQL_USER,
 } = process.env;
 
-console.log(path.join(__dirname, "./migration/*.ts"));
-
-console.log(
-  DB,
-  ELEPHANTSQL_DB,
-  ELEPHANTSQL_HOST,
-  ELEPHANTSQL_PASSWORD,
-  ELEPHANTSQL_PORT,
-  ELEPHANTSQL_USER,
-  "<== process.env"
-)
-
-export const AppDataSource = DB=='elephantSQL' 
-  ? new DataSource({
-    type: "postgres",
-    host: ELEPHANTSQL_HOST,
-    username: ELEPHANTSQL_USER,
-    database: ELEPHANTSQL_DB,
-    password: ELEPHANTSQL_PASSWORD,
-    port: Number(ELEPHANTSQL_PORT),
-    entities: [User, Post],
-    synchronize: true,
-    logging: !__prod__,
-    migrations: [path.join(__dirname, "./migration/*.js")],
-  })
-  :new DataSource({
-  type: "postgres",
-  host: "localhost",
-  port: 5432,
-  username: "postgres",
-  database: "liredit",
-  password: "12345",
-  entities: [User, Post],
-  synchronize: true,
-  logging: !__prod__,
-  migrations: [path.join(__dirname, "./migration/*.js")],
-});
+export const AppDataSource =
+  DB == "elephantSQL"
+    ? new DataSource({
+        type: "postgres",
+        host: ELEPHANTSQL_HOST,
+        username: ELEPHANTSQL_USER,
+        database: ELEPHANTSQL_DB,
+        password: ELEPHANTSQL_PASSWORD,
+        port: Number(ELEPHANTSQL_PORT),
+        entities: [User, Post],
+        synchronize: true,
+        logging: !__prod__,
+        migrations: [path.join(__dirname, "./migration/*.js")],
+      })
+    : new DataSource({
+        type: "postgres",
+        host: "localhost",
+        port: 5432,
+        username: "postgres",
+        database: "liredit",
+        password: "12345",
+        entities: [User, Post],
+        synchronize: true,
+        logging: !__prod__,
+        migrations: [path.join(__dirname, "./migration/*.js")],
+      });
