55ff6f57275388fe4ca37c6332622003327c9fa5
XavierFabregat <xavi.fabregat.pous@gmail.com>
2023-07-04 18:58:31 +0200

    WIP [FE/Fix]: Fix cookies header

--- client/src/utils/createUrqlClient.ts
+++ client/src/utils/createUrqlClient.ts
@@ -91,6 +91,10 @@ console.log("URL: ", url);
 export const createUrqlClient = (ssrExchange: any, ctx: any) => {
   let cookie = "";
   if (isServer()) {
+    console.log("ctx: ", ctx);
+    console.log("ctx.req: ", ctx.req);
+    console.log("headers: ", ctx.req?.headers);
+    console.log("cookie: ", ctx.req?.headers?.cookie);
     cookie = ctx?.req?.headers?.cookie;
   }
 
